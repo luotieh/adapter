dd74e2781b6032c50ffc810f43afb2514dc5107f
luotie <luotieh@gmail.com>
2026-01-30 10:03:47 +0800

    patch:fix deps.py, about deepsoc init

--- src/adapter/deps.py
+++ src/adapter/deps.py
@@ -14,4 +14,4 @@ def get_flowshadow_client() -> FlowShadowClient:
     return FlowShadowClient(str(settings.flowshadow_base_url), settings.flowshadow_api_key)
 
 def get_deepsoc_client() -> DeepSOCClient:
-    return DeepSOCClient(str(settings.deepsoc_base_url), settings.deepsoc_api_key)
+    return DeepSOCClient()
